1d6761b9ffb4bd676ffa7f7e8fc69b13cdcb4f3e
Aymane Kharj <140598129+xaymnn@users.noreply.github.com>
2024-03-24 14:08:39 +0100

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.esm_javastocks {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.esm_javastocks to javafx.fxml;
-    exports com.example.esm_javastocks;
-}
